b8c42f5e325ceee45b61bfcfd348afefb0c96f90
Steve <steven.d.foster@gmail.com>
2021-03-24 23:53:23 -0400

    Updating to 1.0.4 because of MySql support

--- HavokMultimedia.Utilities.Console/Version.cs
+++ HavokMultimedia.Utilities.Console/Version.cs
@@ -18,6 +18,6 @@ namespace HavokMultimedia.Utilities.Console
 {
     public static class Version
     {
-        public static string Value => "1.0.3";
+        public static string Value => "1.0.4";
     }
 }
