93b6b81254ddc10bb8f2d915ab6003864cd08257
Steve <steven.d.foster@gmail.com>
2021-04-02 17:08:28 -0400

    Added wget command

---
+++ HavokMultimedia.Utilities.Console/Commands/WGet.cs
@@ -0,0 +1,60 @@
+﻿// /*
+// Copyright (c) 2021 Steven Foster (steven.d.foster@gmail.com)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// */
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+namespace HavokMultimedia.Utilities.Console.Commands
+{
+    public class WGet : Command
+    {
+        protected override void CreateHelp(CommandHelpBuilder help)
+        {
+            help.AddSummary("Same as WGET command for getting web resources");
+            help.AddValue("<source URL> <output file>");
+        }
+
+        protected override void Execute()
+        {
+            var sourceURL = GetArgValues().GetAtIndexOrDefault(0).TrimOrNull();
+            log.Debug($"{nameof(sourceURL)}: {sourceURL}");
+            if (sourceURL == null) throw new ArgsException(nameof(sourceURL), $"{nameof(sourceURL)} not provided");
+
+            var outputFile = GetArgValues().GetAtIndexOrDefault(1).TrimOrNull();
+            log.Debug($"{nameof(outputFile)}: {outputFile}");
+            if (outputFile == null)
+            {
+                outputFile = sourceURL.Split('/').TrimOrNull().WhereNotNull().LastOrDefault();
+
+                outputFile = Util.FilenameSanitize(outputFile, "_");
+                outputFile = Path.Combine(Environment.CurrentDirectory, outputFile);
+            }
+            outputFile = Path.GetFullPath(outputFile);
+            log.Debug($"{nameof(outputFile)}: {outputFile}");
+            DeleteExistingFile(outputFile);
+
+            using (var cli = new WebClient())
+            {
+                cli.DownloadFile(sourceURL, outputFile);
+
+                log.Info(sourceURL + "  ->  " + outputFile);
+
+            }
+
+        }
+    }
+}
--- HavokMultimedia.Utilities.Console/Version.cs
+++ HavokMultimedia.Utilities.Console/Version.cs
@@ -18,6 +18,6 @@ namespace HavokMultimedia.Utilities.Console
 {
     public static class Version
     {
-        public static string Value => "1.3.4";
+        public static string Value => "1.3.5";
     }
 }
