b7db16db06c6ab3240c30f37f23b20aa190ced8b
Steve <steven.d.foster@gmail.com>
2021-04-02 15:54:52 -0400

    Fixed logging issue to correctly log to our logger

--- HavokMultimedia.Utilities.Console/Commands/WebServer.cs
+++ HavokMultimedia.Utilities.Console/Commands/WebServer.cs
@@ -14,20 +14,76 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using EmbedIO;
+using Swan;
+using Swan.Logging;
+
 namespace HavokMultimedia.Utilities.Console.Commands
 {
     public class WebServer : Command
     {
+        private class SwanLogger : Swan.Logging.ILogger
+        {
+            public Swan.Logging.LogLevel LogLevel { get; init; }
+            private readonly ILogger log;
+            private SwanLogger(Swan.Logging.LogLevel logLevel, ILogger log)
+            {
+                this.LogLevel = logLevel;
+                this.log = log;
+            }
+
+            public static IEnumerable<Swan.Logging.ILogger> CreateLoggers(ILogger log)
+            {
+                foreach (var type in Util.GetEnumItems<Swan.Logging.LogLevel>())
+                {
+                    yield return new SwanLogger(type, log);
+                }
+            }
+
+            public void Dispose() { }
+
+            public void Log(LogMessageReceivedEventArgs logEvent)
+            {
+                if (logEvent.MessageType != LogLevel) return;
+                switch (LogLevel)
+                {
+                    case Swan.Logging.LogLevel.None:
+                        break;
+                    case Swan.Logging.LogLevel.Trace:
+                        if (logEvent.Exception == null) log.Trace(logEvent.Message); else log.Trace(logEvent.Message, logEvent.Exception);
+                        break;
+                    case Swan.Logging.LogLevel.Debug:
+                        if (logEvent.Exception == null) log.Debug(logEvent.Message); else log.Debug(logEvent.Message, logEvent.Exception);
+                        break;
+                    case Swan.Logging.LogLevel.Info:
+                        if (logEvent.Exception == null) log.Info(logEvent.Message); else log.Info(logEvent.Message, logEvent.Exception);
+                        break;
+                    case Swan.Logging.LogLevel.Warning:
+                        if (logEvent.Exception == null) log.Warn(logEvent.Message); else log.Warn(logEvent.Message, logEvent.Exception);
+                        break;
+                    case Swan.Logging.LogLevel.Error:
+                        if (logEvent.Exception == null) log.Error(logEvent.Message); else log.Error(logEvent.Message, logEvent.Exception);
+                        break;
+                    case Swan.Logging.LogLevel.Fatal:
+                        if (logEvent.Exception == null) log.Critical(logEvent.Message); else log.Critical(logEvent.Message, logEvent.Exception);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
         protected override void CreateHelp(CommandHelpBuilder help)
         {
-            help.AddSummary("Creates a web server");
+            help.AddSummary("Creates a web server to host static html files");
             help.AddParameter("ipaddress", "ip", "IP address to bind to (localhost)");
             help.AddParameter("port", "o", "Port to bind to (8080)");
+            help.AddDetail("It is recommended to have an index.html file in the root of the directory");
             help.AddValue("<directory to serve>");
         }
+
 
         protected override void Execute()
         {
@@ -37,6 +93,10 @@ namespace HavokMultimedia.Utilities.Console.Commands
             log.Debug($"{nameof(directoryToServe)}: {directoryToServe}");
             directoryToServe = Path.GetFullPath(directoryToServe);
             if (!Directory.Exists(directoryToServe)) throw new DirectoryNotFoundException("Directory was not found " + directoryToServe);
+            log.Debug($"{nameof(directoryToServe)}: {directoryToServe}");
+
+            Logger.NoLogging();
+            foreach (var logger in SwanLogger.CreateLoggers(log)) Logger.RegisterLogger(logger);
 
             var server = new EmbedIO.WebServer(o => o.WithUrlPrefix($"http://{ipaddress}:{port}").WithMode(HttpListenerMode.EmbedIO));
             server = server.WithLocalSessionManager();
@@ -45,7 +105,9 @@ namespace HavokMultimedia.Utilities.Console.Commands
 
             using (server)
             {
+                log.Debug("WebServer starting...");
                 server.RunAsync();
+                log.Debug("WebServer started");
                 log.Info("Server running, press ESC or Q to quit, serving files from " + directoryToServe);
                 while (true)
                 {
