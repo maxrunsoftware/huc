1cf4e0be9be53449ee7ba953bb2064000b5cea29
Steve <steven.d.foster@gmail.com>
2021-04-14 20:24:54 -0400

    Working on encryption stuff

---
+++ HavokMultimedia.Utilities.Console/Commands/FileDecrypt.cs
@@ -0,0 +1,67 @@
+﻿/*
+Copyright (c) 2021 Steven Foster (steven.d.foster@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace HavokMultimedia.Utilities.Console.Commands
+{
+    public class FileDecrypt : Command
+    {
+        protected override void CreateHelp(CommandHelpBuilder help)
+        {
+            help.AddSummary("Decrypts a file");
+            help.AddValue("<private key file> <file to decrypt> <decrypted file>");
+        }
+
+        protected override void Execute()
+        {
+            var values = GetArgValues().TrimOrNull().WhereNotNull().ToList();
+
+            var privateKeyFile = values.GetAtIndexOrDefault(0);
+            log.Debug($"{nameof(privateKeyFile)}: {privateKeyFile}");
+            if (privateKeyFile == null) throw new ArgsException(nameof(privateKeyFile), $"No {nameof(privateKeyFile)} specified");
+            privateKeyFile = Path.GetFullPath(privateKeyFile);
+            CheckFileExists(privateKeyFile);
+            log.Debug($"{nameof(privateKeyFile)}: {privateKeyFile}");
+            var privateKey = ReadFile(privateKeyFile, Constant.ENCODING_UTF8_WITHOUT_BOM);
+
+            var fileToDecrypt = values.GetAtIndexOrDefault(1);
+            log.Debug($"{nameof(fileToDecrypt)}: {fileToDecrypt}");
+            if (fileToDecrypt == null) throw new ArgsException(nameof(fileToDecrypt), $"No {nameof(fileToDecrypt)} specified to decrypt");
+            fileToDecrypt = Path.GetFullPath(fileToDecrypt);
+            CheckFileExists(fileToDecrypt);
+            log.Debug($"{nameof(fileToDecrypt)}: {fileToDecrypt}");
+            var fileToDecryptData = Util.FileRead(fileToDecrypt);
+
+            var decryptedFile = values.GetAtIndexOrDefault(2);
+            log.Debug($"{nameof(decryptedFile)}: {decryptedFile}");
+            if (decryptedFile == null) throw new ArgsException(nameof(decryptedFile), $"No {nameof(decryptedFile)} specified to output to");
+            decryptedFile = Path.GetFullPath(decryptedFile);
+            log.Debug($"{nameof(decryptedFile)}: {decryptedFile}");
+
+            using (var rsa = RSA.Create())
+            {
+                rsa.ImportFromPem(privateKey.ToCharArray());
+
+                var decryptedFileData = rsa.Decrypt(fileToDecryptData, RSAEncryptionPadding.OaepSHA512);
+                Util.FileWrite(decryptedFile, decryptedFileData);
+            }
+        }
+    }
+}
---
+++ HavokMultimedia.Utilities.Console/Commands/FileEncrypt.cs
@@ -0,0 +1,67 @@
+﻿/*
+Copyright (c) 2021 Steven Foster (steven.d.foster@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace HavokMultimedia.Utilities.Console.Commands
+{
+    public class FileEncrypt : Command
+    {
+        protected override void CreateHelp(CommandHelpBuilder help)
+        {
+            help.AddSummary("Encrypts a file");
+            help.AddValue("<public key file> <file to encrypt> <encrypted file>");
+        }
+
+        protected override void Execute()
+        {
+            var values = GetArgValues().TrimOrNull().WhereNotNull().ToList();
+
+            var publicKeyFile = values.GetAtIndexOrDefault(0);
+            log.Debug($"{nameof(publicKeyFile)}: {publicKeyFile}");
+            if (publicKeyFile == null) throw new ArgsException(nameof(publicKeyFile), $"No {nameof(publicKeyFile)} specified");
+            publicKeyFile = Path.GetFullPath(publicKeyFile);
+            CheckFileExists(publicKeyFile);
+            log.Debug($"{nameof(publicKeyFile)}: {publicKeyFile}");
+            var publicKey = ReadFile(publicKeyFile, Constant.ENCODING_UTF8_WITHOUT_BOM);
+
+            var fileToEncrypt = values.GetAtIndexOrDefault(1);
+            log.Debug($"{nameof(fileToEncrypt)}: {fileToEncrypt}");
+            if (fileToEncrypt == null) throw new ArgsException(nameof(fileToEncrypt), $"No {nameof(fileToEncrypt)} specified to encrypt");
+            fileToEncrypt = Path.GetFullPath(fileToEncrypt);
+            CheckFileExists(fileToEncrypt);
+            log.Debug($"{nameof(fileToEncrypt)}: {fileToEncrypt}");
+            var fileToEncryptData = Util.FileRead(fileToEncrypt);
+
+            var encryptedFile = values.GetAtIndexOrDefault(2);
+            log.Debug($"{nameof(encryptedFile)}: {encryptedFile}");
+            if (encryptedFile == null) throw new ArgsException(nameof(encryptedFile), $"No {nameof(encryptedFile)} specified to output to");
+            encryptedFile = Path.GetFullPath(encryptedFile);
+            log.Debug($"{nameof(encryptedFile)}: {encryptedFile}");
+
+            using (var rsa = RSA.Create())
+            {
+                rsa.ImportFromPem(publicKey.ToCharArray());
+
+                var encryptedFileData = rsa.Encrypt(fileToEncryptData, RSAEncryptionPadding.OaepSHA512);
+                Util.FileWrite(encryptedFile, encryptedFileData);
+            }
+        }
+    }
+}
