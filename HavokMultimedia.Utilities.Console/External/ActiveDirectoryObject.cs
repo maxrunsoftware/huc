a86fd337b41d336dbdb7a60480300cfc374c2f1a
Steve <steven.d.foster@gmail.com>
2021-04-17 21:46:52 -0400

    Fixed copyright

--- HavokMultimedia.Utilities.Console/External/ActiveDirectoryObject.cs
+++ HavokMultimedia.Utilities.Console/External/ActiveDirectoryObject.cs
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright (c) 2020 Steven Foster (steven.d.foster@gmail.com)
+Copyright (c) 2021 Steven Foster (steven.d.foster@gmail.com)
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
