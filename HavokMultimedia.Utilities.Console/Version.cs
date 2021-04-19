1a2d7b50e636a581e9af7134c0b8c05207eac30f
Steve <steven.d.foster@gmail.com>
2021-04-18 20:14:59 -0400

    Version update to 1.8.0 for ActiveDirectory commands

--- HavokMultimedia.Utilities.Console/Version.cs
+++ HavokMultimedia.Utilities.Console/Version.cs
@@ -18,6 +18,6 @@ namespace HavokMultimedia.Utilities.Console
 {
     public static class Version
     {
-        public static string Value => "1.7.2";
+        public static string Value => "1.8.0";
     }
 }
