b55fe3292cfe5ed5bb1c50b5bc98eae267a59c3f
Steve <steven.d.foster@gmail.com>
2021-06-02 20:25:01 -0400

    Added DirectorySize command

---
+++ HavokMultimedia.Utilities.Console/Commands/DirectorySize.cs
@@ -0,0 +1,80 @@
+﻿/*
+Copyright (c) 2021 Steven Foster (steven.d.foster@gmail.com)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HavokMultimedia.Utilities.Console.Commands
+{
+    public class DirectorySize : Command
+    {
+        protected override void CreateHelp(CommandHelpBuilder help)
+        {
+            help.AddSummary("Scans all fills and subfolders of a directory to obtain the size of the directory");
+            help.AddValue("<target directory>");
+            help.AddExample("MyDirectory");
+        }
+
+        protected override void ExecuteInternal()
+        {
+            var targetDirectory = GetArgValueTrimmed(0);
+            if (targetDirectory == null) throw new ArgsException(nameof(targetDirectory), $"No <{nameof(targetDirectory)}> specified");
+            targetDirectory = Path.GetFullPath(targetDirectory);
+            log.Debug($"{nameof(targetDirectory)}: {targetDirectory}");
+            if (!Directory.Exists(targetDirectory)) throw new ArgsException(nameof(targetDirectory), $"<{nameof(targetDirectory)}> does not exist {targetDirectory}");
+
+            log.Debug("Scanning for files: " + targetDirectory);
+            var files = Util.FileListFiles(targetDirectory, recursive: true).ToList();
+            log.Debug($"Found {files.Count} files");
+
+            long totalSize = 0;
+            foreach (var file in files)
+            {
+                var size = Util.FileGetSize(file);
+                log.Trace(size.ToString().PadLeft(long.MaxValue.ToString().Length + 1) + " " + file);
+                totalSize += size;
+            }
+            var sizes = new List<(long size, string suffix)>
+            {
+                (Constant.BYTES_TERA, "TB"),
+                (Constant.BYTES_GIGA, "GB"),
+                (Constant.BYTES_MEGA, "MB"),
+                (Constant.BYTES_KILO, "KB"),
+                (-1, "B"),
+            };
+
+
+            foreach (var sz in sizes)
+            {
+                if (totalSize < 1)
+                {
+                    log.Info("0");
+                    break;
+                }
+                if (totalSize >= sz.size)
+                {
+                    var newsize = (double)totalSize / (double)sz.size;
+                    newsize = newsize.Round(MidpointRounding.AwayFromZero, 2);
+                    log.Info(newsize + " " + sz.suffix);
+                    break;
+                }
+            }
+
+        }
+    }
+}
