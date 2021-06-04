0455093f024cbfa9d24ab564c4112001ba3c20d5
Steve <steven.d.foster@gmail.com>
2021-06-03 23:33:12 -0400

    Some code cleanup

--- HavokMultimedia.Utilities.Console/External/LdapEntryAttributeCollection.cs
+++ HavokMultimedia.Utilities.Console/External/LdapEntryAttributeCollection.cs
@@ -44,19 +44,18 @@ namespace HavokMultimedia.Utilities.Console.External
 
             foreach (DictionaryEntry da in attributes)
             {
-                if (da.Value is DirectoryAttribute directoryAttribute)
-                {
-                    var name = directoryAttribute.Name.TrimOrNull();
-                    if (name == null) continue;
+                if (da.Value is not DirectoryAttribute directoryAttribute) continue;
 
-                    AddItems(name, directoryAttribute);
-                }
+                var name = directoryAttribute.Name.TrimOrNull();
+                if (name == null) continue;
+
+                AddItems(name, directoryAttribute);
             }
 
-            Guid? ObjectGUID = null;
+            Guid? objectGUID = null;
             var objectGuidBytes = GetByteArray("objectGUID");
-            if (objectGuidBytes != null && objectGuidBytes.Length > 0) ObjectGUID = Ldap.Bytes2Guid(objectGuidBytes);
-            if (ObjectGUID != null) this.ObjectGUID = ObjectGUID.Value;
+            if (objectGuidBytes != null && objectGuidBytes.Length > 0) objectGUID = Ldap.Bytes2Guid(objectGuidBytes);
+            if (objectGUID != null) ObjectGUID = objectGUID.Value;
             DistinguishedName = GetString("distinguishedName");
 
             // Parsing ranged attributes
@@ -73,7 +72,12 @@ namespace HavokMultimedia.Utilities.Console.External
                 }
 
                 var blockSize = 0;
-                if (!dictionary.TryGetValue(range.name, out var values)) dictionary.Add(range.name, values = new List<LdapEntryAttributeValue>());
+
+                if (!dictionary.TryGetValue(range.name, out var values))
+                {
+                    values = new List<LdapEntryAttributeValue>();
+                    dictionary.Add(range.name, values);
+                }
                 values.AddRange(dictionary[attributeName]);
 
                 while (range.rangeEnd != null)
@@ -83,32 +87,40 @@ namespace HavokMultimedia.Utilities.Console.External
                     var end = range.rangeEnd.Value + blockSize;
                     var attributeFilter = range.name + ";range=" + start + "-" + end;
 
-                    if (ObjectGUID != null) entry = ldap.SearchResultEntryGetByObjectGuid(ObjectGUID.Value, new LdapQueryConfig(attributes: attributeFilter.Yield()));
-                    else if (DistinguishedName != null) entry = ldap.SearchResultEntryGetByDistinguishedName(DistinguishedName, new LdapQueryConfig(attributes: attributeFilter.Yield()));
+                    if (objectGUID != null)
+                    {
+                        entry = ldap.SearchResultEntryGetByObjectGuid(objectGUID.Value, new LdapQueryConfig(attributes: attributeFilter.Yield()));
+                    }
+                    else if (DistinguishedName != null)
+                    {
+                        entry = ldap.SearchResultEntryGetByDistinguishedName(DistinguishedName, new LdapQueryConfig(attributes: attributeFilter.Yield()));
+                    }
                     else break;
+
                     attributes = entry.Attributes;
                     if (attributes == null) break;
 
                     var valuesBefore = values.Count;
                     foreach (DictionaryEntry da in attributes)
                     {
-                        if (da.Value is DirectoryAttribute directoryAttribute)
+                        if (da.Value == null) continue;
+                        if (da.Value is not DirectoryAttribute directoryAttribute) continue;
+
+                        var name = directoryAttribute.Name.TrimOrNull();
+                        if (name == null) continue;
+                        if (!name.StartsWith(range.name + ";range=", StringComparison.OrdinalIgnoreCase)) continue;
+
+                        if (!dictionary.TryGetValue(name, out var rangeList))
                         {
-                            var name = directoryAttribute.Name.TrimOrNull();
-                            if (name == null) continue;
-                            if (!name.StartsWith(range.name + ";range=", StringComparison.OrdinalIgnoreCase)) continue;
-
-                            if (!dictionary.TryGetValue(name, out var rangeList)) dictionary.Add(name, rangeList = new List<LdapEntryAttributeValue>());
-                            foreach (var obj in directoryAttribute)
-                            {
-                                var sreav = LdapEntryAttributeValue.Parse(obj);
-                                if (sreav != null) rangeList.Add(sreav);
-                            }
-
-                            values.AddRange(rangeList);
-
-                            range = ParseRange(name);
+                            rangeList = new List<LdapEntryAttributeValue>();
+                            dictionary.Add(name, rangeList);
                         }
+                        rangeList.AddRange(LdapEntryAttributeValue.Parse(directoryAttribute));
+
+                        values.AddRange(rangeList);
+
+                        range = ParseRange(name);
+
                     }
                     if (values.Count == valuesBefore) break; // sanity check
                 }
@@ -142,12 +154,7 @@ namespace HavokMultimedia.Utilities.Console.External
 
         private void AddItems(string name, DirectoryAttribute directoryAttribute)
         {
-            var values = new List<LdapEntryAttributeValue>();
-            foreach (var obj in directoryAttribute)
-            {
-                var sreav = LdapEntryAttributeValue.Parse(obj);
-                if (sreav != null) values.Add(sreav);
-            }
+            var values = LdapEntryAttributeValue.Parse(directoryAttribute);
 
             if (dictionary.TryGetValue(name, out var list))
             {
@@ -187,87 +194,27 @@ namespace HavokMultimedia.Utilities.Console.External
         }
 
         public IEnumerable<string> GetStrings(string name) => GetList(name).Select(o => o.String).WhereNotNull();
-
         public string GetString(string name) => GetStrings(name).FirstOrDefault();
 
         public IEnumerable<byte[]> GetByteArrays(string name) => GetList(name).Select(o => o.Bytes).WhereNotNull();
-
         public byte[] GetByteArray(string name) => GetByteArrays(name).FirstOrDefault();
 
         public IEnumerable<long> GetLongs(string name) => GetList(name).Where(o => o.Long.HasValue).Select(o => o.Long.Value);
-
-        public long? GetLong(string name)
-        {
-            foreach (var item in GetList(name))
-            {
-                if (item.Long.HasValue)
-                {
-                    return item.Long;
-                }
-            }
-            return null;
-        }
+        public long? GetLong(string name) => GetLongs(name).FirstOrNull();
 
         public IEnumerable<int> GetInts(string name) => GetList(name).Where(o => o.Int.HasValue).Select(o => o.Int.Value);
-
-        public int? GetInt(string name)
-        {
-            foreach (var item in GetList(name))
-            {
-                if (item.Int.HasValue)
-                {
-                    return item.Int;
-                }
-            }
-            return null;
-        }
+        public int? GetInt(string name) => GetInts(name).FirstOrNull();
 
         public IEnumerable<uint> GetUInts(string name) => GetList(name).Where(o => o.UInt.HasValue).Select(o => o.UInt.Value);
-
-        public uint? GetUInt(string name)
-        {
-            foreach (var item in GetList(name))
-            {
-                if (item.UInt.HasValue)
-                {
-                    return item.UInt;
-                }
-            }
-            return null;
-        }
+        public uint? GetUInt(string name) => GetUInts(name).FirstOrNull();
 
         public IEnumerable<bool> GetBools(string name) => GetList(name).Where(o => o.Bool.HasValue).Select(o => o.Bool.Value);
-
-        public bool? GetBool(string name)
-        {
-            foreach (var item in GetList(name))
-            {
-                if (item.Bool.HasValue)
-                {
-                    return item.Bool;
-                }
-            }
-            return null;
-        }
+        public bool? GetBool(string name) => GetBools(name).FirstOrNull();
 
         public IEnumerable<DateTime> GetDateTimeUTCs(string name) => GetList(name).Where(o => o.DateTimeUtc.HasValue).Select(o => o.DateTimeUtc.Value);
+        public DateTime? GetDateTimeUTC(string name) => GetDateTimeUTCs(name).FirstOrNull();
 
-        public DateTime? GetDateTimeUTC(string name)
-        {
-            foreach (var item in GetList(name))
-            {
-                if (item.DateTimeUtc.HasValue)
-                {
-                    return item.DateTimeUtc;
-                }
-            }
-            return null;
-        }
-
-        public bool IsCollection(string name)
-        {
-            return GetList(name).Count > 1;
-        }
+        public bool IsCollection(string name) => GetList(name).Count > 1;
 
         #endregion Get
 
