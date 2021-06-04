dce4923a700a4343e44c000b811d2f900234523e
Steve <steven.d.foster@gmail.com>
2021-06-04 18:16:54 -0400

    v1.10.8 for code base changes

--- HavokMultimedia.Utilities.Console/Version.cs
+++ HavokMultimedia.Utilities.Console/Version.cs
@@ -18,6 +18,6 @@ namespace HavokMultimedia.Utilities.Console
 {
     public static class Version
     {
-        public static string Value => "1.10.7";
+        public static string Value => "1.10.8";
     }
 }
