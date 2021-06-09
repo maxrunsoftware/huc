b40bdd40be81d88d799e20a8a6b7fe5334f08c1d
Steve <steven.d.foster@gmail.com>
2021-06-09 19:15:30 -0400

    Added CopyOnWrite Bucket back

--- HavokMultimedia.Utilities/Buckets.cs
+++ HavokMultimedia.Utilities/Buckets.cs
@@ -138,6 +138,71 @@ namespace HavokMultimedia.Utilities
     }
 
     /// <summary>
+    /// Threadsafe cache implementation implementing copy on write using backing dictionary factory and value generation function.
+    /// Precache elements using the Cache function
+    /// </summary>
+    /// <typeparam name="TKey">Key</typeparam>
+    /// <typeparam name="TValue">Generated Value</typeparam>
+    public class BucketCacheThreadSafeCopyOnWrite<TKey, TValue> : IBucketReadOnly<TKey, TValue>
+    {
+        private readonly object locker = new object();
+        private readonly Func<TKey, TValue> factory;
+        private readonly Func<IDictionary<TKey, TValue>> dictionaryFactory;
+        private IReadOnlyDictionary<TKey, TValue> dictionary; // shouldn't need volatile because of memory barrier of lock(locker)
+        public IEnumerable<TKey> Keys => dictionary.Keys;
+
+        public BucketCacheThreadSafeCopyOnWrite(Func<TKey, TValue> factory, Func<IDictionary<TKey, TValue>> dictionaryFactory)
+        {
+            this.factory = factory.CheckNotNull(nameof(factory));
+            this.dictionaryFactory = dictionaryFactory.CheckNotNull(nameof(dictionaryFactory));
+            this.dictionary = dictionaryFactory().AsReadOnly();
+        }
+
+        public BucketCacheThreadSafeCopyOnWrite(Func<TKey, TValue> factory) : this(factory, () => new Dictionary<TKey, TValue>()) { }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                if (dictionary.TryGetValue(key, out var val)) return val;
+                lock (locker)
+                {
+                    if (dictionary.TryGetValue(key, out val)) return val;
+                    var d = dictionaryFactory();
+                    foreach (var kvp in dictionary) d.Add(kvp.Key, kvp.Value);
+                    val = factory(key);
+                    d.Add(key, val);
+                    dictionary = d.AsReadOnly();
+                    return val;
+                }
+            }
+        }
+
+        public void Cache(IEnumerable<TKey> keys)
+        {
+            lock (locker)
+            {
+                var d = dictionaryFactory();
+                foreach (var kvp in dictionary) d.Add(kvp.Key, kvp.Value);
+                foreach (var key in keys)
+                {
+                    var val = factory(key);
+                    d[key] = val;
+                }
+                dictionary = d.AsReadOnly();
+            }
+        }
+
+        public void Clear()
+        {
+            lock (locker)
+            {
+                this.dictionary = dictionaryFactory().AsReadOnly();
+            }
+        }
+    }
+
+    /// <summary>
     /// Storage for multiple buckets based on a name
     /// </summary>
     /// <typeparam name="TKey">Key type</typeparam>
