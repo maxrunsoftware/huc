4d62e5177b915d9bdfb108d4dba488cd99f348af
Steve <steven.d.foster@gmail.com>
2021-07-05 16:08:49 -0400

    Version update for project rename

--- MaxRunSoftware.Utilities.Console/Version.cs
+++ MaxRunSoftware.Utilities.Console/Version.cs
@@ -18,6 +18,6 @@ namespace MaxRunSoftware.Utilities.Console
 {
     public static class Version
     {
-        public static string Value => "1.11.2";
+        public static string Value => "1.12.0";
     }
 }
