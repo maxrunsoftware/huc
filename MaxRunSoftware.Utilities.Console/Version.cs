c9a91863b48b105a6bea6204ff7ae4d94d47d22a
Steve <steven.d.foster@gmail.com>
2021-07-16 19:22:24 -0400

    Version to 1.12.3 for Zip bug fix

--- MaxRunSoftware.Utilities.Console/Version.cs
+++ MaxRunSoftware.Utilities.Console/Version.cs
@@ -18,6 +18,6 @@ namespace MaxRunSoftware.Utilities.Console
 {
     public static class Version
     {
-        public static string Value => "1.12.2";
+        public static string Value => "1.12.3";
     }
 }
