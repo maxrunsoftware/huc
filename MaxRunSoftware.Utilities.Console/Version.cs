04feafdfb8959eb7e58ee9e33eb197de6935e78b
Steve <steven.d.foster@gmail.com>
2021-10-05 03:41:37 -0400

    Version update for EncodePassword

--- MaxRunSoftware.Utilities.Console/Version.cs
+++ MaxRunSoftware.Utilities.Console/Version.cs
@@ -18,6 +18,6 @@ namespace MaxRunSoftware.Utilities.Console
 {
     public static class Version
     {
-        public static string Value => "1.12.9";
+        public static string Value => "1.12.10";
     }
 }
