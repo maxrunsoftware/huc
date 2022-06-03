5b419bb236d2437f98ad15234721dd77f4eb6907
Steve <steven.d.foster@gmail.com>
2022-06-03 15:34:10 -0400

    Working on Bucket tests

---
+++ MaxRunSoftware.Utilities.Tests/BucketTests.cs
@@ -0,0 +1,57 @@
+﻿namespace MaxRunSoftware.Utilities.Tests;
+
+public class BucketReadOnlyTests
+{
+    [Fact]
+    public void Testing()
+    {
+        var cgf = new CacheGenFunc();
+
+        var bro = new BucketCache<string, string>(cgf.GenVal);
+        Assert.Equal(0, cgf.TimesCalled);
+        Assert.Empty(bro.Keys);
+
+        var val = bro["a"];
+        Assert.Equal("Va", val);
+        Assert.Equal(1, cgf.TimesCalled);
+        Assert.Single(bro.Keys);
+
+        val = bro["a"];
+        Assert.Equal("Va", val);
+        Assert.Equal(1, cgf.TimesCalled);
+        Assert.Single(bro.Keys);
+
+        val = bro["b"];
+        Assert.Equal("Vb", val);
+        Assert.Equal(2, cgf.TimesCalled);
+        Assert.Equal(2, bro.Keys.Count());
+
+        val = bro["a"];
+        Assert.Equal("Va", val);
+        Assert.Equal(2, cgf.TimesCalled);
+        Assert.Equal(2, bro.Keys.Count());
+
+        val = bro["n"];
+        Assert.Null(val);
+        Assert.Equal(3, cgf.TimesCalled);
+        Assert.Equal(3, bro.Keys.Count());
+
+        val = bro["n"];
+        Assert.Null(val);
+        Assert.Equal(3, cgf.TimesCalled);
+        Assert.Equal(3, bro.Keys.Count());
+    }
+
+    private class CacheGenFunc
+    {
+        public int TimesCalled { get; set; }
+        public string? GenVal(string key)
+        {
+            TimesCalled++;
+            if (key == "n") return null;
+            return "V" + key;
+        }
+    }
+
+
+}
